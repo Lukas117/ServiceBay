a20776084236e0f5a67177d1f0d4723c39486821
Marton Vadasz <75419452+VADMARCI@users.noreply.github.com>
2021-11-11 11:40:12 +0100

    Revert "idk"
    This reverts commit 428dc7ec51b4e25f681079a2bc5cf5d77d3a9189.

---
+++ ServiceBay/ServiceBay/Services/IBidObserver.cs
@@ -0,0 +1,13 @@
+﻿using ServiceBay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceBay.Services
+{
+    public interface IBidObserver
+    {
+        public void updateBid(Bid bid);
+    }
+}
