f737204873f81704786d21e745a6d82e0f0dc61c
I3ali <71490447+I3ali@users.noreply.github.com>
2021-11-11 10:50:52 +0100

    BidAPI controller

---
+++ ServiceBay/ServiceBay/Controllers/MvcBidController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServiceBay.Models;
+
+namespace ServiceBay.Controllers
+{
+    public class MvcBidController : Controller
+    {
+        public IActionResult Index()
+        {
+            IEnumerable<Bid> bid = null;
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("https://localhost:5001/api/ApiBid");
+
+            var consumeapi = hc.GetAsync("ApiBid");
+            consumeapi.Wait();
+
+            var readdata = consumeapi.Result;
+            if (readdata.IsSuccessStatusCode)
+            {
+                var displaydata = readdata.Content.ReadAsAsync<IList<Bid>>();
+                displaydata.Wait();
+
+                bid = displaydata.Result;
+            }
+            return View(bid);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Bid inserttemp)
+        {
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("https://localhost:5001/api/ApiBid");
+
+            var insertrecord = hc.PostAsJsonAsync<Bid>("ApiAuction", inserttemp);
+            insertrecord.Wait();
+
+            var savedata = insertrecord.Result;
+            if (savedata.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+    }
+}
