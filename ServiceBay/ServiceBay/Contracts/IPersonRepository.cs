7b81fcba6069618080ef2bff5f6cc23d9e181e33
Lukas117 <lukykoll@gmail.com>
2021-11-18 11:37:38 +0100

    get person

---
+++ ServiceBay/ServiceBay/Contracts/IPersonRepository.cs
@@ -0,0 +1,13 @@
+﻿using ServiceBay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceBay.Contracts
+{
+    public interface IPersonRepository
+    {
+        public Task<Person> GetPerson(int id);
+    }
+}
---
+++ ServiceBay/ServiceBay/Repository/PersonRepository.cs
@@ -0,0 +1,24 @@
+﻿using ServiceBay.Contracts;
+using ServiceBay.Data;
+using ServiceBay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceBay.Repository
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PersonRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Person> GetPerson(int id)
+        {
+            return await _context.Person.FindAsync(id);
+        }
+    }
+}
