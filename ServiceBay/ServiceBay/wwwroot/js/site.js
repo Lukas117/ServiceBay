6c36817e158f65fb5479169577fae9e13cdb2f57
Marton Vadasz <75419452+VADMARCI@users.noreply.github.com>
2021-11-23 10:57:22 +0100

    notif icon and stuff

--- ServiceBay/ServiceBay/wwwroot/js/site.js
+++ ServiceBay/ServiceBay/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function GetAuc(id) {
+    return id;
+}
+
+function UpdateNotifications() {
+    var auc1 = GetAuc(24);
+    //var prev = auc1.Price();
+    var auc2 = setTimeout(GetAuc(24), 2000);
+    //var newp = auc2.Price();
+    if (auc1 != auc2) {
+        console.log("you are smart");
+    }
+    else {
+        console.log("you may be smart...");
+    }
+}
+
+GetAuc();
